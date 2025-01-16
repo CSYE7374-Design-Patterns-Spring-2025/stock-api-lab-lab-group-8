82939702a8148deb84f29b34039c7e99d93ae1d6
TirthNaik-99 <naik.ti@northeastern.edu>
2025-01-15 19:54:24 -0500

    created and implemented Stock class

---
+++ src/main/java/edu/neu/csye7374/Stock.java
@@ -0,0 +1,32 @@
+package edu.neu.csye7374;
+
+public abstract class Stock implements Tradable {
+    private String name;
+    private double price;
+    private String description;
+
+    public Stock(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String toString() {
+        return "Stock{name='" + this.name + "', price=" + this.price + ", description='" + this.description + "'}";
+    }
